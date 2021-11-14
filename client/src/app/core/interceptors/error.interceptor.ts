5e6a2650de1fdcb96af715b0abada07051698198
shwan <shwan.m19@gmail.com>
2021-11-13 21:01:31 -0600

    Section 12

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -11,6 +11,7 @@ export class ErrorInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
+      delay(1000),
       catchError(error => {
         if (error) {
           
